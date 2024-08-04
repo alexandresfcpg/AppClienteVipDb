c5a36799d7ac7e1bacbb476688f2ec361cc979a3
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-04 16:30:06 -0300

    Criação da tabela cliente para testar a funcionalidade.

--- app/src/main/java/app/daazi/aluno/appclientevipdb/api/AppDataBase.java
+++ app/src/main/java/app/daazi/aluno/appclientevipdb/api/AppDataBase.java
@@ -1,8 +1,10 @@
 package app.daazi.aluno.appclientevipdb.api;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -22,6 +24,27 @@ public class AppDataBase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Criar as tabelas
+        String sqlTabelaCliente = "CREATE TABLE cliente (\n" +
+                "id      INTEGER PRIMARY KEY AUTOINCREMENT, \n" +
+                "nome    TEXT, \n" +
+                "email   TEXT, \n" +
+                "status  INTEGER, \n" +
+                "datainc TEXT, \n" +
+                "dataalt TEXT \n" +
+                ")";
+
+
+        try{
+
+            db.execSQL(sqlTabelaCliente);
+
+            Log.i(AppUtil.LOG_APP, "TB Cliente: "+sqlTabelaCliente);
+
+        }catch (SQLException e){
+
+            Log.e(AppUtil.LOG_APP, "Erro TB Cliente: "+e.getMessage());
+
+        }
     }
 
     @Override
